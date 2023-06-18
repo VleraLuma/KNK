ac24d7b3b8007e75377be2b0b2f59e76c13d3241
RinesaHoxha <122171723+RinesaHoxha@users.noreply.github.com>
2023-06-18 23:58:16 +0200

    Update I18N.java
    librarit e nevojshme

---
+++ src/main/java/com/example/sample/Translate/I18N.java
@@ -0,0 +1,14 @@
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.StringBinding;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.concurrent.Callable;
